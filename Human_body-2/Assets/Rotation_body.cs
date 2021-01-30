04853e24721469ca03eb445b0711616b805334e0
MorshedSayeed <78271544+MorshedSayeed@users.noreply.github.com>
2021-01-31 03:01:00 +0600

    First Commit
    Added Unity Project to Github

---
+++ Human_body-2/Assets/Rotation_body.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_body : MonoBehaviour
+{
+     public float mSpeed;
+	// Start is called before the first frame update
+    void Start()
+    {
+       mSpeed = 30f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f , mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+	transform.Rotate(10f* Time.deltaTime, 2f , 30f * Time.deltaTime);
+    }
+}
